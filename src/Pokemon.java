6cda596df819230e3d48b1382ded68480ff72e01
Jonah Colagross <colagjon000@battlegroundps.org>
2019-09-09 07:30:41 -0700

    Formatting changes

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,10 +1,8 @@
 
 public class Pokemon {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Pikachu welcomes you to the world of Pokemon!");
 		System.out.println("(\\__/)\n(o^.^)\nz(_(\")(\")");
 	}
-
 }
